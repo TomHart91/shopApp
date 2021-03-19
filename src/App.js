2150e083525b7ab661aed061180496e4b068ed8c
tomhart91 <tomhart91@gmail.com>
2021-03-19 11:07:06 +0000

    Updated with the login system

--- src/App.js
+++ src/App.js
@@ -1,12 +1,18 @@
-import React, { Fragment } from "react";
+import React, { useState } from "react";
 import "./index.css"
 import { BrowserRouter as Router, Route, Link, Switch, Redirect } from "react-router-dom";
 
-import Products from './productPage'
-import Charts from './charts'
+import Products from './productPage';
+import Charts from './charts';
+import Login from'./login';
 
 export default function App() {
-  const name = 'John Doe'
+  const [token, setToken] = useState();
+
+  if(!token) {
+    return <Login setToken={setToken} />
+  }
+
   return (
     <Router>
     <main>
@@ -14,11 +20,13 @@ export default function App() {
         <ul>
           <li><a href="/">Products</a></li>
           <li><a href="/contact">Charts</a></li>
+          <li><a href="/login">Login</a></li>
         </ul>
         </nav>
        
     <Route path="/" exact component={Products} />
     <Route path="/contact"  component={Charts} />
+    <Route path="/login"  component={Login} />
      </main>
      </Router>
     
