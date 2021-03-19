d3b33f63526cf2cb6815b7a3379cb222eb29da74
tomhart91 <tomhart91@gmail.com>
2021-03-19 11:33:24 +0000

    fixed something wrong with the ogin system

--- src/App.js
+++ src/App.js
@@ -5,13 +5,29 @@ import { BrowserRouter as Router, Route, Link, Switch, Redirect } from "react-ro
 import Products from './productPage';
 import Charts from './charts';
 import Login from'./login';
+import useToken from './useToken';
+
 
 export default function App() {
-  const [token, setToken] = useState();
+
+  const { token, setToken } = useToken();
+
+  // function setToken(userToken) {
+  //   sessionStorage.setItem('token', JSON.stringify(userToken));
+  // }
+
+
+function getToken() {
+  const tokenString = sessionStorage.getItem('token');
+  const userToken = JSON.parse(tokenString);
+  return userToken?.token
+}
 
   if(!token) {
     return <Login setToken={setToken} />
   }
+
+  // const token = getToken();
 
   return (
     <Router>
